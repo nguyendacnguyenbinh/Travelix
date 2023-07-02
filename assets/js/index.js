96168b61c6387ba9c39f07fb3283bb147763172b
Nguyen Dac Nguyen Binh <nguyendacnguyenbinh@gmail.com>
2023-07-02 20:58:52 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+$ = document.querySelector.bind(document);
+$$ = document.querySelectorAll.bind(document);
+
+
+//scoll/sticky
+
+const headerTop = $('.header-topbar')
+const headerNav = $('.header-nav')
+
+window.addEventListener('scroll', scrollFunction)
+
+function scrollFunction () {
+    if((document.documentElement.getClientRects()[0].y) <= -20){
+        headerTop.classList.add('hide')
+        headerNav.classList.add('scroll')
+    } else {
+        headerTop.classList.remove('hide')
+        headerNav.classList.remove('scroll')
+    }
+}
+
+//active
+
+const headerNavItem = $$('.header-nav__item')
+
+headerNavItem.forEach(item => {
+    item.addEventListener('click', (e)=>{
+        e.preventDefault()
+        $('.header-nav__item.active').classList.remove('active')
+        item.classList.add('active')
+    })
+});
+
+//header search input
+
+const headerSearchIcon = $('.header-nav__search i')
+const headerSearchInput = $('.header-nav__search-input')
+
+headerSearchIcon.addEventListener('click', (e)=> {
+    headerSearchInput.classList.toggle('hide')
+})
+
+
+// container header list active
+const searchItem = $$('.search-header__item')
+
+searchItem.forEach(item => {
+    item.addEventListener('click', (e)=>{
+        e.preventDefault()
+        $('.search-header__item.active').classList.remove('active')
+        item.classList.add('active')
+    })
+});
